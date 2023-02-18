f58399ff01afd1fd8a5ac5051980ec7b3350cdf4
Yang <freda.y.zhao@gmail.com>
2023-02-18 15:25:54 -0500

    first scene

--- basic_scene/script.js
+++ basic_scene/script.js
@@ -1,31 +1,31 @@
-//Scene
-const scene = new THREE.Scene()
-
-// Red cube
-const geometry = new THREE.BoxGeometry(1,1,1)
-const material = new THREE.MeshBasicMaterial({color:0xff0000})
-const mesh = new THREE.Mesh(geometry,material)
-
-// Add to scene
-scene.add(mesh)
+// Canvas
+const canvas = document.querySelector('.webgl')
 
 // Sizes
-const sizes ={
-    width:800,
-    height:600
+const sizes = {
+    width: 800,
+    height: 600
 }
 
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+const cubeMaterial = new THREE.MeshBasicMaterial({
+    color: '#ff0000'
+})
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
+scene.add(cubeMesh)
+
 // Camera
-const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height)
-camera.position.z=3
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
 scene.add(camera)
 
 // Renderer
-const canvas = document.querySelector('.webgl')
-// console.log(canvas)
 const renderer = new THREE.WebGLRenderer({
-    canvas:canvas
+    canvas: canvas
 })
-
-renderer.setSize(sizes.width,sizes.height)
-
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
