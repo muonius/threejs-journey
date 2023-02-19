bb1d6b19e00957024ddec225ac0dc2c358dbba2d
Yang <freda.y.zhao@gmail.com>
2023-02-19 16:40:35 -0500

    animation

---
+++ 06-animations/src/script_clock.js
@@ -0,0 +1,68 @@
+import * as THREE from 'three'
+import gsap from 'gsap'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+// Clock
+
+const clock = new THREE.Clock()
+
+//Animations
+
+const tick =()=> {
+// console.log('tick')
+// window.requestAnimationFrame(tick)
+
+// Clock
+
+const elapsedTime = clock.getElapsedTime()
+
+// Update Objects
+// mesh.rotation.y = elapsedTime  * Math.PI * 2
+// make cube circle rotation
+// mesh.position.y = Math.sin(elapsedTime)
+// mesh.position.x = Math.cos(elapsedTime)
+
+camera.position.y = Math.sin(elapsedTime)
+camera.position.x = Math.cos(elapsedTime)
+
+camera.lookAt(mesh.position)
+
+
+// mesh.position.x += 0.01
+
+// Render
+renderer.render(scene, camera)
+
+window.requestAnimationFrame(tick)
+
+}
+
+tick()
---
+++ 06-animations/src/script_time.js
@@ -0,0 +1,61 @@
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+// Time
+
+
+let time=Date.now()
+
+//Animations
+
+const tick =()=> {
+// console.log('tick')
+// window.requestAnimationFrame(tick)
+
+// Time
+const currentTime = Date.now()
+// deltaTime is frame by frame difference
+// rotating is at the same time regardless of the frame rate
+const deltaTime = currentTime - time
+time = currentTime
+
+// Update Objects
+mesh.rotation.y +=0.001 * deltaTime
+// mesh.position.x += 0.01
+
+// Render
+renderer.render(scene, camera)
+
+window.requestAnimationFrame(tick)
+
+}
+
+tick()
